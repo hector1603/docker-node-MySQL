ad536b97e2c010c3ed0f7352ab36d0b15b06f1f2
Hector <hector.ajumado@gmail.com>
2025-09-29 15:56:20 -0400

    Cambia los valores de las directivas en docker-compose.yml

--- backend-node-docker/src/index.js
+++ backend-node-docker/src/index.js
@@ -1,7 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT;
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
+
+console.log("Clave secreta: ", jwtSecret);
+console.log("Puerto: ", port);
 
 app.get('/', (req, res) => {
     res.send('API BackEnd Funcionando');
